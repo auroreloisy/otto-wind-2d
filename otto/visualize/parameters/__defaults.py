98dd019ed4f42246e1b3e2a3e8b2ea4cb9881c30
aurore.loisy <aurore.loisy@gmail.com>
2022-10-28 14:55:30 +0200

    typo

--- otto/visualize/parameters/__defaults.py
+++ otto/visualize/parameters/__defaults.py
@@ -4,7 +4,7 @@ R_BAR = 2.5
 # Policy
 POLICY = 0  # -1=RL, O=infotaxis, 1=space-aware infotaxis, 5=random, 6=greedy, 7=mean distance, 8=voting, 9=mls
 MODEL_PATH = None  # saved model for POLICY=-1, e.g., "../learn/models/20220201-230054/20220201-230054_model"
-ALPHAVEC_PATH = None  # save Perseus policy for POLICY=-2, e.g., "../../perseus/vf_rate_5.0_gamma_0.98_ic2_it_21_shaping_factor_0.1_shaping_power_1.0_nb_45000_epsilon_0.0_v2.pkl"
+ALPHAVEC_PATH = None  # saved Perseus policy for POLICY=-2, e.g., "../../perseus/vf_rate_5.0_gamma_0.98_ic2_it_21_shaping_factor_0.1_shaping_power_1.0_nb_45000_epsilon_0.0_v2.pkl"
 STEPS_AHEAD = 1  # number of anticipated moves, can be > 1 only for POLICY=0
 # Setup
 DRAW_SOURCE = True  # if False, episodes will continue until the source is almost surely found (Bayesian setting)
